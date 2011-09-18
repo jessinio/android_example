5154c8aeeb04a2c77a78a685a4b11c36b2e7213d
jessinio.liang <jessinio@gmail.com>
2011-09-18 13:51:32 +0800

    add UIOverlay example

---
+++ TestUIOverlay/src/com/jessinio/TestVisibility/TestVisibilityActivity.java
@@ -0,0 +1,13 @@
+package com.jessinio.TestVisibility;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestVisibilityActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
